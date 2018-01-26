6d0bebb8589eb58994e48a481d691e0a39c8ede2
mlmlmlml121 <mlmlmlml121@cntv.cn>
2018-01-26 22:00:40 +0800

    Update and rename web/config.php to  web/config.php

--- web/config.php
+++ web/config.php
@@ -1,7 +1,7 @@
 <?php
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyBuMFc5qGOO3eRW0nOnCLU6nkeNl5vLcZ0');
-define('GJ_CODE', 'jp');
+define('GJ_CODE', 'HK');
 define('SITE_NAME', 'U2B');
 define('TITLENAME', 'U2B');
 define('EN2DEKEY', '.FDC|"9$9X~A9r^|>1l-gFU]nQ3,FvLYDM[T~hRo5y;jdo#ZId|M^[M|J.~XXx$');
